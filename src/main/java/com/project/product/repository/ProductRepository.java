eba6551061ff27df064b79048f4390022c02d048
Masud Rana <masud.rana@apsissolutions.com>
2024-10-21 19:11:05 +0200

    issue resolved

--- src/main/java/com/project/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.product.repository;
-
-import com.project.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
